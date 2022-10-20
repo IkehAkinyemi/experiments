4135535b7845d22223fef5227cd3231f72dcaef4
IkehAkinyemi <mrikehchukwuka@gmail.com>
2022-10-20 03:37:40 +0100

    Mutexes & data race

--- go/mutexes/mutexe.go
+++ go/mutexes/mutexe.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"strconv"
+	"sync"
+)
+
+type balance struct {
+	amount   float64
+	currency string
+	mu       sync.RWMutex
+}
+
+var mu = &sync.Mutex{}
+var mybalance = &balance{amount: 50.00, currency: "GBP"}
+
+func (b *balance) Add(i float64) {
+	b.mu.Lock()
+	b.amount += i
+	b.mu.Unlock()
+}
+
+func (b *balance) Display() string {
+	b.mu.RLock()
+	defer b.mu.RUnlock()
+	return strconv.FormatFloat( b.amount, 'f', 2, 64) + " " + b.currency
+}
+
 func main() {
-	
 }
