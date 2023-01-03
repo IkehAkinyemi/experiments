adac99b9eed526498a968d71954b5477bd21e76f
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-01-03 23:55:24 +0100

    Mocking compose and pipe function in TS

--- TypeScript/compose/index.ts
+++ TypeScript/compose/index.ts
@@ -15,20 +15,20 @@ const func2 = (v: string) => `func2(${v})`;
 const func3 = (v: string) => `func3(${v})`;
 
 
-export const pipe = <T extends any[], R>(
-  fn1: (...args: T) => R,
-  ...fns: Array<(a: R) => R>
-) => {
-  const piped = fns.reduce((prevFn, nextFn) => (value: R) => nextFn(prevFn(value)), value => value);
-  return (...args: T) => piped(fn1(...args));
-};
+// export const pipe = <T extends any[], R>(
+//   fn1: (...args: T) => R,
+//   ...fns: Array<(a: R) => R>
+// ) => {
+//   const piped = fns.reduce((prevFn, nextFn) => (value: R) => nextFn(prevFn(value)), value => value);
+//   return (...args: T) => piped(fn1(...args));
+// };
 
-const pipedFunction = pipe(func1, func2, func3);
-console.log(pipedFunction("value1", "value2"));
+// const pipedFunction = pipe(func1, func2, func3);
+// console.log(pipedFunction("value1", "value2"));
 
 
-// export const pipe = <T>(fn1: (a: T) => T, ...fns: Array<(a: T) => T>) =>
-//   fns.reduce((prevFn, nextFn) => value => nextFn(prevFn(value)), fn1); 
+export const pipe = <T>(fn1: (a: T) => T, ...fns: Array<(a: T) => T>) =>
+  fns.reduce((prevFn, nextFn) => value => nextFn(prevFn(value)), fn1); 
 
 
 
@@ -36,14 +36,14 @@ console.log(pipedFunction("value1", "value2"));
 // const compose = <T>(fn1: (a: T) => T, ...fns: Array<(a: T) => T>) =>
 // fns.reduce((prevFn, nextFn) => value => prevFn(nextFn(value)), fn1); 
 
-// const compose = <T extends any[], U>(
-//   fn1: (...args: T) => U,
-//   ...fns: Array<(x: U) => U>
-// ) => {
-//   const composed = fns.reduce((prevFn, nextFn) => (value: U) => prevFn(nextFn(value)), value => value);
-//   return (...args: T) => composed(fn1(...args));
-// }
+const compose = <T extends any[], U>(
+  fn1: (...args: T) => U,
+  ...fns: Array<(x: U) => U>
+) => {
+  const composed = fns.reduce((prevFn, nextFn) => (value: U) => prevFn(nextFn(value)), value => value);
+  return (...args: T) => composed(fn1(...args));
+}
 
 
-// const composedFunction = compose(func1, func2, func3);
-// console.log(composedFunction("value1", "value2"));
+const composedFunction = compose(func1, func2, func3);
+console.log(composedFunction("value1", "value2"));
