d856d4963f8577320a36fdef2c01d05296a14924
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-01-24 22:55:58 +0100

    relearning python

---
+++ python/hello_world.py
@@ -0,0 +1,6 @@
+magicians = ['alice', 'david', 'carolina', 1]
+is_true =  "alic" not in magicians
+for magician in magicians:
+  print(f"{magician.title()}, that was a great trick!") 
+print(f"I can't wait to see your next trick, {magician.title()}.\n")
+alien_0 = {9: 'green', 'points': 5}
