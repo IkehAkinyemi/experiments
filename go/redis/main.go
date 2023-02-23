6546bd14a358aff7cb737293bb9ffd8497d96b82
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-02-23 23:26:23 +0100

    experiment with redis go- client

---
+++ go/redis/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+
+	"encoding/json"
+
+	"github.com/go-redis/redis"
+)
+
+type Author struct {
+	Name string `json:"name"`
+	Age int `json:"age"`
+}
+
+func main() {
+    client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		Password: "",
+		DB: 0,
+    })
+    
+    json, err := json.Marshal(Author{Name: "Elliot", Age: 25})
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    err = client.Set("id1234", json, 0).Err()
+    if err != nil {
+        fmt.Println(err)
+    }
+    val, err := client.Get("id1234").Result()
+    if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println(val)
+}
