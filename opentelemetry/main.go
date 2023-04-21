8f861b84f212632090da32868d0c96306e4e9652
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-04-21 23:52:07 +0100

    Setup opentelemetry

---
+++ opentelemetry/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"opentelemetry/tracing"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.opentelemetry.io/contrib/instrumentation/go.mongodb.org/mongo-driver/mongo/otelmongo"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+)
+
+var client *mongo.Client
+
+func main() {
+	tp, tpErr := tracing.TraceProvider()
+	if tpErr != nil {
+		log.Fatal(tpErr)
+	}
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+	connectMongo()
+	startWebServer()
+}
+
+func connectMongo() {
+	opts := options.Client()
+
+	//mongo OTEL instrumentation
+	opts.Monitor = otelmongo.NewMonitor()
+	opts.ApplyURI("mongodb://localhost:27017")
+	client, _ = mongo.Connect(context.Background(), opts)
+
+	//seed the database with some todo's
+	docs := []interface{}{
+		bson.D{{Key: "id", Value: "1"}, {Key: "title", Value: "Buy groceries"}},
+		bson.D{{Key: "id", Value: "2"}, {Key: "title", Value: "install Aspecto.io"}},
+		bson.D{{Key: "id", Value: "3"}, {Key: "title", Value: "Buy dogz.io domain"}},
+	}
+	client.Database("todo").Collection("todos").InsertMany(context.Background(), docs)
+}
+
+func startWebServer() {
+	r := gin.Default()
+
+	//gin OTEL instrumentation
+	r.Use(otelgin.Middleware("todo-service"))
+
+	r.GET("/todo", func(c *gin.Context) {
+		collection := client.Database("todo").Collection("todos")
+
+		//make sure to pass c.Request.Context() as the context and not c itself
+		cur, findErr := collection.Find(c.Request.Context(), bson.D{})
+		if findErr != nil {
+			c.AbortWithError(500, findErr)
+			return
+		}
+		results := make([]interface{}, 0)
+		curErr := cur.All(c, &results)
+		if curErr != nil {
+			c.AbortWithError(500, curErr)
+			return
+		}
+		c.JSON(http.StatusOK, results)
+	})
+	_ = r.Run(":8080")
+}
