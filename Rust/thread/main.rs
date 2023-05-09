74c5e10f22cae5cf0e23aa393cba9bab78c5715d
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-05-09 22:46:05 +0100

    threads in Rust

--- Rust/thread/main.rs
+++ Rust/thread/main.rs
@@ -4,4 +4,13 @@ fn main() {
     let handle = thread::spawn(|| {
         // code to be executed in the new thread
     });
+    
+    match handle.join() {
+        Ok(result) => {
+            // handle success case with result
+        },
+        Err(_) => {
+            // handle error case
+        }
+    };
 }
