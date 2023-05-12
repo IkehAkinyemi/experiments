6047af4a23bcb84fb60b23a9cf56668e1335746f
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-05-12 11:40:06 +0100

    uncommitted changes

---
+++ go/unbuffered-channel/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func writeToFile(filename string, ch chan string) {
+    file, err := os.Create(filename)
+    if err != nil {
+        fmt.Printf("Error creating file: %s", err)
+        return
+    }
+    defer file.Close()
+
+    for {
+        message := <-ch
+        if message == "" {
+            return
+        }
+        _, err = file.WriteString(message + "\n")
+        if err != nil {
+            fmt.Printf("Error writing to file: %s", err)
+            return
+        }
+    }
+}
+
+func main() {
+    ch := make(chan string)
+
+    go writeToFile("output.txt", ch)
+
+    ch <- "Hello, world!"
+    ch <- "This is a test message."
+    ch <- ""
+
+    fmt.Println("File written successfully!")
+}
