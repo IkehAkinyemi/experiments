6eac8de8254773123967d5f56a37fc962658c349
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-05-24 09:03:13 +0100

    add a new arc code

--- Rust/arc/main.rs
+++ Rust/arc/main.rs
@@ -1,25 +1,45 @@
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
+use std::fs::File;
+use std::io::{self, BufRead};
 use std::thread;
 
 fn main() {
-    let shared_data = Arc::new(42);
+    let file_paths = vec!["file1.txt", "file2.txt", "file3.txt"];
+    let shared_data = Arc::new(Mutex::new(Vec::new()));
 
-    let handle = thread::spawn({
-        let shared_data = shared_data.clone();
+    let mut handles = vec![];
 
-        move || {
-            // use the shared_data value in the new thread
-        }
-    });
+    for file_path in file_paths {
+        let shared_data = Arc::clone(&shared_data);
+        let handle = thread::spawn(move || {
+            // Open the file
+            let file = File::open(file_path).expect("Failed to open file");
 
-    // do some work in the main thread...
+            // Read the contents of the file
+            let lines: Vec<String> = io::BufReader::new(file)
+                .lines()
+                .map(|line| line.expect("Failed to read line"))
+                .collect();
 
-    let result = match handle.join() {
-        Ok(result) => {
-            // handle success case with result
-        },
-        Err(_) => {
-            // handle error case
-        }
-    };
+            // Lock the shared data
+            let mut data = shared_data.lock().unwrap();
+
+            // Append the lines to the shared data
+            data.extend(lines);
+        });
+
+        handles.push(handle);
+    }
+
+    // Wait for all threads to finish
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    // Explicitly drop the Arc
+    drop(shared_data);
+
+    // Accessing the shared data after dropping the Arc would result in a compile-time error
+    // Uncomment the following line to see the error:
+    // let data = shared_data.lock().unwrap();
 }
