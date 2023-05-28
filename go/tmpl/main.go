905a5c48ce234294b3c57fb04f3dcd73435304df
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-05-28 18:21:06 +0100

    still learning

--- go/tmpl/main.go
+++ go/tmpl/main.go
@@ -3,23 +3,17 @@ package main
 import (
 	"html/template"
 	"net/http"
-	"time"
 )
 
-func formDate(t time.Time) string {
-	layout := "2006-01-02"
-	return t.Format(layout)
-}
-
 func process(w http.ResponseWriter, r *http.Request) {
-	funcMap := template.FuncMap{"fdate": formDate}
-	t:= template.New("tmpl.html").Funcs(funcMap)
+	t:= template.New("tmpl.html")
 	t, err := t.ParseFiles("tmpl.html", "t2.html")
 	if err != nil {
 		http.Error(w, "error occurred", http.StatusInternalServerError)
 		return
 	}
-	t.Execute(w, time.Now())
+	c := `I asked: <i>"What's up?"</i>`
+	t.Execute(w, c)
 }
 
 func main() {
